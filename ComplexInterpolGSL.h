ab6ae06c108eb47aaa55b230084199ad464b2585
rrausch <rrausch@physnet.uni-hamburg.de>
2020-04-08 15:50:23 +0900

    Changes to be committed: 	modified:   ComplexInterpolGSL.h
    - kill_splines() hinzugefuegt

--- ComplexInterpolGSL.h
+++ ComplexInterpolGSL.h
@@ -23,7 +23,18 @@ struct ComplexInterpol
 		return data[0].integrate() + 1.i * data[1].integrate();
 	};
 	
+	complex<double> evaluate (double x)
+	{
+		return data[0].evaluate(x) + 1.i * data[1].evaluate(x);
+	};
+	
 	std::array<Interpol<GSL>,2> data; // array indices: real and imaginary part
+	
+	void kill_splines()
+	{
+		data[0].kill_splines();
+		data[1].kill_splines();
+	}
 };
 
 #endif
